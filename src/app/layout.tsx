ebc94bd5c61310ae5b78ffc0ef23e4b83ed21648
Enzo Miguel Fernández Tobares <enzomiguelfernandez43@gmail.com>
2026-06-04 15:07:52 -0300

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar"; // Importamos tu Navbar
+import ScrollbarProvider from '@/components/ScrollbarProvider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,9 +32,11 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Navbar /> {/* Inyectamos la navegación arriba de todo */}
-        <main className="flex-grow">
-          {children}
-        </main>
+        <ScrollbarProvider>
+          <main className="flex-grow">
+            {children}
+          </main>
+        </ScrollbarProvider>
       </body>
     </html>
   );
---
+++ src/components/ScrollbarProvider.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+import 'overlayscrollbars/overlayscrollbars.css';
+
+export default function ScrollbarProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <OverlayScrollbarsComponent
+      element="div"
+      style={{ height: '100vh', width: '100%' }}
+      options={{ scrollbars: { autoHide: 'scroll' } }}
+      defer
+    >
+      {children}
+    </OverlayScrollbarsComponent>
+  );
+}
