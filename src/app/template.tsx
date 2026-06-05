db6860f4dd41ecac25d7ea999ac3664f7a673820
Enzo Miguel Fernández Tobares <enzomiguelfernandez43@gmail.com>
2026-06-05 00:18:28 -0300

    Update template.tsx

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,9 +5,9 @@ import { motion } from 'framer-motion'
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ y: 20, opacity: 0 }} // Empieza un poquito más abajo y transparente
-      animate={{ y: 0, opacity: 1 }}   // Sube a su posición original y se vuelve opaco
-      transition={{ ease: 'easeInOut', duration: 0.5 }} // Duración del efecto
+      initial={{ opacity: 0, scale: 0.99 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ ease: 'easeInOut', duration: 0.5 }}
     >
       {children}
     </motion.div>
